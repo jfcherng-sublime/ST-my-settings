083cc7dca391325cab41f6a2745e262b31b15805
Jack Cherng <jfcherng@gmail.com>
2019-03-24 04:38:35 +0800

    Use AutoSetIndentation from Package Control
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- Packages/AutoSetIndentation/problem_files/guess_indent.js
+++
@@ -1,8 +0,0 @@
-// https://github.com/SublimeTextIssues/Core/issues/1640
-// Guess settings from buffer: Detected as Tab/4 but should be Space/2
-
-function(keys, values, rereduce) {
-  keys.forEach(function(key) {
-
-  })
-}
