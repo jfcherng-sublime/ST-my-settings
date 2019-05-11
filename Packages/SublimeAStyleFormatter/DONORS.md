d970ac7b40557e3a6ed6a5972c4075a91024988b
Jack Cherng <jfcherng@gmail.com>
2019-05-12 03:38:14 +0800

    Remove packages from repo which will be auto downloaded
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- Packages/SublimeAStyleFormatter/DONORS.md
+++
@@ -1,9 +0,0 @@
-# Donors
-
-Here is a list of donors, thanks all for your contribution/contributions!
-
-(If you would like your entry to be updated or be removed, please contact me)
-
-* [Davis Clark](https://github.com/jdc0589)
-* Zhong Ying
-* Conner Bryan
--- Packages/SublimeAStyleFormatter/src/user_build_py3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Absolute path to this script, e.g. /home/user/bin/foo.sh
-SCRIPT=$(readlink "$0")
-# Absolute path this script is in, thus /home/user/bin
-SCRIPTPATH=$(dirname "${SCRIPT}")
-
-/bin/bash "${SCRIPTPATH}/_build.sh" --python="${PYTHON:-python3.3}" "$@"
