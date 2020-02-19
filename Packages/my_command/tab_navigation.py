f2f78140bbdc706255aefc29fcb91688d594454d
Jack Cherng <jfcherng@gmail.com>
2020-02-19 21:13:40 +0800

    Better tabs/groups nav cycling
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ Packages/my_command/tab_navigation.py
@@ -0,0 +1,27 @@
+"""
+Modified from https://github.com/ahuff44/sublime-better-tab-cycling
+
+Keybinding example:
+
+// go to the previous tab view
+{ "keys": ["ctrl+super+j"], "command": "step_tab_cycle", "args": { "steps": -1 } },
+// go to the next tab view
+{ "keys": ["ctrl+super+l"], "command": "step_tab_cycle", "args": { "steps": 1 } },
+"""
+
+import sublime
+import sublime_plugin
+
+
+class StepTabCycleCommand(sublime_plugin.WindowCommand):
+    """
+    Switch to the next tab in the active pane.
+    Like sublime's builtin next_view, but stays within the active pane
+    """
+
+    def run(self, steps: int) -> None:
+        window = self.window
+
+        group_index, view_index = window.get_view_index(window.active_view())
+        views = window.views_in_group(group_index)
+        window.focus_view(views[(view_index + steps) % len(views)])
