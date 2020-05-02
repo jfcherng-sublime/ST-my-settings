1649d4123b4c5d4ed9dd2f5b4329654ecdd29fe3
Jack Cherng <jfcherng@gmail.com>
2020-05-03 03:44:14 +0800

    feat: update decorate_inline_comment.py
    Support comments in neighbor lines.
    
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- Packages/my_command/decorate_inline_comment.py
+++ Packages/my_command/decorate_inline_comment.py
@@ -2,105 +2,164 @@ import sublime
 import sublime_plugin
 import re
 
+COMMENT_SCOPE = "comment"
+
+# fmt: off
+CMT_CONFIGS = [
+    {
+        'modifier': '//',
+        'fill': '/',
+        'fill_with_padding_spaces': False,
+        're_whitespaces': None,
+    },
+    {
+        'modifier': '::',
+        'fill': '-',
+        'fill_with_padding_spaces': True,
+        're_whitespaces': None,
+    },
+    {
+        'modifier': '#',
+        'fill': '-',
+        'fill_with_padding_spaces': True,
+        're_whitespaces': None,
+    },
+    {
+        'modifier': ';',
+        'fill': '-',
+        'fill_with_padding_spaces': True,
+        're_whitespaces': None,
+    },
+]
+# fmt: on
+
+for cmt_config in CMT_CONFIGS:
+    cmt_config["re_whitespaces"] = re.compile(
+        r"^(?P<leading_ws>\s*)(?={})".format(cmt_config["modifier"])
+    )
+
 
 class decorateInlineCommentCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         v = self.view
 
-        # fmt: off
-        cmtConfigs = [
-            {
-                'modifier': '//',
-                'fill': '/',
-                'fillWithPaddingSpaces': False,
-                'reWhitespaces': None,
-            },
-            {
-                'modifier': '#',
-                'fill': '-',
-                'fillWithPaddingSpaces': True,
-                'reWhitespaces': None,
-            },
-        ]
-        # fmt: on
-
-        for cmtConfig in cmtConfigs:
-            cmtConfig["reWhitespaces"] = re.compile(r"^(\s*)({})".format(cmtConfig["modifier"]))
-
-        v.run_command("expand_selection", {"to": "scope"})
-
         for sel in v.sel():
-            maxLength = 0
-            lines = v.lines(sel)
+            # sel.b is the visual cursor point
+            sel = self._get_full_comment_region(v, sel.b)
 
-            for lineRegion in lines:
-                lineText = v.substr(lineRegion)
+            if not sel:
+                continue
 
-                cmtConfigMatched = None
-                for cmtConfig in cmtConfigs:
-                    matches = cmtConfig["reWhitespaces"].match(lineText)
+            line_regions = v.lines(sel)
+
+            for line_region in line_regions:
+                for cmt_config in CMT_CONFIGS:
+                    matches = cmt_config["re_whitespaces"].match(v.substr(line_region))
                     if matches is not None:
-                        cmtConfigMatched = cmtConfig
                         break
 
-                if cmtConfigMatched is None:
-                    raise Exception("No matched comment style...")
+            if not matches:
+                print("Unsupported comment: {!r}".format(v.substr(sel)))
+                continue
 
-                leadingWS = matches.group(1)
-                maxLength = max(maxLength, lineRegion.size())
-
-            lineLength = maxLength - len(leadingWS + cmtConfigMatched["modifier"])
+            leading_ws = matches.group("leading_ws")
+            max_length = max(map(lambda l: len(l), line_regions))
+            line_length = max_length - len(leading_ws + cmt_config["modifier"])
 
             # "expand selection to scope" may not select the trailing \n
             # in some syntaxes and we try to compensate it here
-            lastCharInScope = v.substr(sublime.Region(sel.end() - 1, sel.end()))
-            if lastCharInScope != "\n":
-                leadingEol = "\n"
-                endingEol = ""
+            if v.substr(sublime.Region(sel.end() - 1, sel.end())) == "\n":
+                eols = ("", "\n")
             else:
-                leadingEol = ""
-                endingEol = "\n"
+                eols = ("\n", "")
 
-            if cmtConfigMatched["fillWithPaddingSpaces"]:
-                midContentForBeginEndLines = " " + cmtConfigMatched["fill"] * (lineLength - 1) + " "
+            if cmt_config["fill_with_padding_spaces"]:
+                dummy_content = " " + cmt_config["fill"] * (line_length - 1) + " "
             else:
-                midContentForBeginEndLines = cmtConfigMatched["fill"] * (lineLength + 1)
+                dummy_content = cmt_config["fill"] * (line_length + 1)
 
             # insert ending line
             v.insert(
                 edit,
                 sel.end(),
                 (
-                    leadingEol
-                    + leadingWS
-                    + cmtConfigMatched["modifier"]
-                    + midContentForBeginEndLines
-                    + cmtConfigMatched["modifier"]
-                    + endingEol
+                    eols[0]
+                    + leading_ws
+                    + cmt_config["modifier"]
+                    + dummy_content
+                    + cmt_config["modifier"]
+                    + eols[1]
                 ),
             )
 
-            for lineRegion in reversed(lines):
-                line = v.substr(lineRegion)
+            for line_region in reversed(line_regions):
+                line = v.substr(line_region)
 
                 # 1 for at least 1 extra trailing whitespace
-                rPadding = 1 + (maxLength - lineRegion.size())
+                padding_r = 1 + (max_length - line_region.size())
 
-                prefix = leadingWS + cmtConfigMatched["modifier"]
-                postfix = (" " * rPadding) + cmtConfigMatched["modifier"]
-                cmtContent = line[len(leadingWS + cmtConfigMatched["modifier"]) :]
+                prefix = leading_ws + cmt_config["modifier"]
+                suffix = (" " * padding_r) + cmt_config["modifier"]
+                cmt_content = line[len(leading_ws + cmt_config["modifier"]) :]
 
-                v.replace(edit, lineRegion, prefix + cmtContent + postfix)
+                v.replace(edit, line_region, prefix + cmt_content + suffix)
 
             # insert begin line
             v.insert(
                 edit,
                 sel.begin(),
                 (
-                    cmtConfigMatched["modifier"]
-                    + midContentForBeginEndLines
-                    + cmtConfigMatched["modifier"]
+                    leading_ws
+                    + cmt_config["modifier"]
+                    + dummy_content
+                    + cmt_config["modifier"]
                     + "\n"
-                    + leadingWS
                 ),
             )
+
+    def _get_full_comment_region(self, view: sublime.View, pt: int) -> sublime.Region:
+        """
+        @brief Get the full comment region with neighbor lines.
+
+        @param view The view
+        @param pt   The point
+
+        @return The full comment region.
+        """
+
+        # hmm... we are not in comment at all
+        if not view.match_selector(pt, COMMENT_SCOPE):
+            return sublime.Region(pt)
+
+        row, col = view.rowcol(pt)
+        row_begin = row_end = row
+        row_max = view.rowcol(view.size())[0]
+
+        # previous lines are comments too?
+        while 0 < row_begin < row_max:
+            test_row = row_begin - 1
+            test_col = len(view.line(view.text_point(test_row, 0)))
+
+            if view.match_selector(view.text_point(test_row, test_col), COMMENT_SCOPE):
+                row_begin = test_row
+            else:
+                break
+
+        # next lines are comments too?
+        while 0 < row_end < row_max:
+            test_row = row_end + 1
+            test_col = len(view.line(view.text_point(test_row, 0)))
+
+            if view.match_selector(view.text_point(test_row, test_col), COMMENT_SCOPE):
+                row_end = test_row
+            else:
+                break
+
+        # fmt: off
+        return view.full_line(
+            sublime.Region(
+                view.text_point(row_begin, col),
+                view.text_point(row_end, col),
+            )
+        )
+        # fmt: on
