9527b119c09155e326955569af864dd0052718ac
Jack Cherng <jfcherng@gmail.com>
2021-04-30 07:33:23 +0800

    Revert "feat: add 0_sublimelinter_markdownlint_live_mode.py"
    This reverts commit fbe4b93e0920a984b6a650f049cd67954787e973.
    
    No longer need this file as of 1.2.2

--- Packages/my_plugin_33/0_sublimelinter_markdownlint_live_mode.py
+++
@@ -1,9 +0,0 @@
-import importlib
-
-try:
-    linter = importlib.import_module("SublimeLinter-contrib-markdownlint.linter").MarkdownLint  # type: ignore
-
-    linter.cmd = ("markdownlint", "${args}", "${temp_file}")
-    linter.tempfile_suffix = "md"
-except Exception as e:
-    print(__file__, e)
