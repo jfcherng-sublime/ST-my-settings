33e31dfaf31436b43a7a3230ae6c913c8c4f851b
Jack Cherng <jfcherng@gmail.com>
2021-07-03 01:37:06 +0800

    fix: sublack with multi project folders
    If we are editing in the 2nd folder, sublack will try to pick
    pyproject.toml from the 1st folder...
    
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ Packages/my_plugin_33/0_sublack.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+import importlib
+import sublime
+
+try:
+    # ------------------------------- #
+    # override utils.find_root_file() #
+    # ------------------------------- #
+
+    utils = importlib.import_module("sublack.sublack").utils  # type: ignore
+
+    find_root_file_original = utils.find_root_file
+
+    def find_root_file_modified(view: sublime.View, filename: str):
+        view_name = view.file_name()
+        window = view.window()
+
+        if not view_name or not window:
+            return None
+
+        folders = window.folders()
+        if not folders:
+            return None
+
+        for folder in sorted(folders, key=len):
+            if view_name.startswith(folder):
+                path = Path(folder) / filename
+                if path.exists():
+                    return path
+
+        return None
+
+    utils.find_root_file = find_root_file_modified
+except Exception as e:
+    print(__file__, e)
