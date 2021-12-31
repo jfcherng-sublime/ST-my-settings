98c1e06db1d51adfcb769c2379002ae48a07928a
Jack Cherng <jfcherng@gmail.com>
2022-01-01 00:05:37 +0800

    feat: add "focus_active_output_panel" command
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ Packages/my_plugin_38/focus_active_output_panel_command.py
@@ -0,0 +1,17 @@
+import sublime_plugin
+
+
+class FocusActiveOutputPanelCommand(sublime_plugin.WindowCommand):
+    def run(self) -> None:
+        if not (panel_name := self.window.active_panel()):
+            return
+
+        self.window.run_command("show_panel", {"panel": panel_name})
+
+        if not panel_name.startswith("output."):
+            return
+
+        panel_name = panel_name[len("output.") :]
+        panel = self.window.find_output_panel(panel_name)
+        assert panel
+        self.window.focus_view(panel)
