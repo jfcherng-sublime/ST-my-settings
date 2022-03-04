a2cd0c029d93dfca3da13808f03ae3513d8714f4
Jack Cherng <jfcherng@gmail.com>
2022-03-04 23:14:52 +0800

    chore: rename async_command_command.py to run_async_command.py
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- Packages/my_plugin_38/async_command_command.py
+++
@@ -1,9 +0,0 @@
-import sublime
-import sublime_plugin
-
-
-class AsyncCommandCommand(sublime_plugin.WindowCommand):
-    """Run a given command asynchronously."""
-
-    def run(self, name: str, args: dict = {}, timeout: float = 0) -> None:
-        sublime.set_timeout_async(lambda: self.window.run_command(name, args), timeout)
---
+++ Packages/my_plugin_38/run_async_command.py
@@ -0,0 +1,9 @@
+import sublime
+import sublime_plugin
+
+
+class RunAsyncCommand(sublime_plugin.WindowCommand):
+    """Run a given command asynchronously."""
+
+    def run(self, command: str, args: dict = {}, timeout_ms: float = 0) -> None:
+        sublime.set_timeout_async(lambda: self.window.run_command(command, args), timeout_ms)
