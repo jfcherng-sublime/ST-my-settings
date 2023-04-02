4245e36dbfb0c7f23738e8f2569f71394f2efc62
Jack Cherng <jfcherng@gmail.com>
2023-04-03 03:09:56 +0800

    chore: bump lsp_utils Electron verion
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ Packages/my_plugin_38/0_lsp_utils.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+import ctypes
+
+try:
+    from lsp_utils import node_runtime
+
+    def get_glibc_version() -> tuple[int, ...] | None:
+        try:
+            libc = ctypes.CDLL("libc.so.6")
+            gnu_get_libc_version = libc.gnu_get_libc_version
+            gnu_get_libc_version.restype = ctypes.c_char_p
+            version_str = gnu_get_libc_version().decode("utf-8")
+            return tuple(map(int, version_str.split(".")))
+        except Exception:
+            return None
+
+    glibc_ver = get_glibc_version()
+
+    # @see https://github.com/electron/electron/releases
+    # Note that although Electron v30 uses Node.js v20,
+    # Electron v29 requires GLIBC_2.18 so it can't run on CentOS 7.9... 😢
+    if not glibc_ver or glibc_ver >= (2, 18):
+        node_runtime.ELECTRON_RUNTIME_VERSION = "30.1.0"
+        node_runtime.ELECTRON_NODE_VERSION = "20.14.0"
+    else:
+        node_runtime.ELECTRON_RUNTIME_VERSION = "28.0.0"
+        node_runtime.ELECTRON_NODE_VERSION = "18.18.2"
+except Exception as e:
+    print(__file__, e)
