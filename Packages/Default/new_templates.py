6589f00eab2320a4243a83428be6d281dc2b25fa
Jun-Fei Cherng <jfcherng@realtek.com>
2023-04-27 14:47:55 +0800

    chore: update ST settings
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- Packages/Default/new_templates.py
+++ Packages/Default/new_templates.py
@@ -1,132 +1,139 @@
 from __future__ import annotations
 
-import os
+import textwrap
+from abc import ABC
+from pathlib import Path
+from typing import Literal
+
 import sublime
 import sublime_plugin
-import textwrap
 
-ST_USER_DIR = os.path.join(sublime.packages_path(), "User")
+ST_USER_DIR = Path(sublime.packages_path()) / "User"
 
 
 def reformat(template: str) -> str:
     return textwrap.dedent(template).lstrip()
 
 
-class NewBuildSystemCommand(sublime_plugin.WindowCommand):
+class AbstractNewFileCommand(sublime_plugin.WindowCommand, ABC):
+    save_path: Path = ST_USER_DIR / "untitled"
+    template: str = ""
+    template_type: Literal["snippet", "text"] = "text"
+
+    @property
+    def save_path_extension(self) -> str:
+        """File extension, without a leading dot, of the save file."""
+        return self.save_path.name.rpartition(".")[2]
+
     def run(self) -> None:
+        self.save_path.parent.mkdir(parents=True, exist_ok=True)
+
         v = self.window.new_file()
-        v.settings().set("default_dir", ST_USER_DIR)
-        v.assign_syntax("Packages/JavaScript/JSON.sublime-syntax")
-        v.set_name("untitled.sublime-build")
+        v.settings().update({
+            "default_dir": str(self.save_path.parent),
+            "default_extension": self.save_path_extension,
+        })
 
-        template = reformat(
-            """
-            {
-            \t"shell_cmd": "${0:make}"
-            }
-            """
-        )
-        v.run_command("insert_snippet", {"contents": template})
+        v.set_name(self.save_path.name)
+        if syntax := sublime.find_syntax_for_file(self.save_path.name):
+            v.assign_syntax(syntax)
 
-
-class NewPluginCommand(sublime_plugin.WindowCommand):
-    def run(self) -> None:
-        v = self.window.new_file()
-        v.settings().set("default_dir", ST_USER_DIR)
-        v.assign_syntax("Packages/Python/Python.sublime-syntax")
-
-        template = reformat(
-            """
-            import sublime
-            import sublime_plugin
+        if self.template_type == "snippet":
+            v.run_command("insert_snippet", {"contents": self.template})
+        elif self.template_type == "text":
+            v.run_command("append", {"characters": self.template})
+        else:
+            raise ValueError(f"Invalid template type: {self.template_type}")
 
 
-            class ExampleCommand(sublime_plugin.TextCommand):
-            \tdef run(self, edit: sublime.Edit) -> None:
-            \t\t$0self.view.insert(edit, 0, "Hello, World!")
-            """
-        )
-        v.run_command("insert_snippet", {"contents": template})
+class NewBuildSystemCommand(AbstractNewFileCommand):
+    save_path: Path = ST_USER_DIR / "untitled.sublime-build"
+    template = reformat("""
+{
+\t"shell_cmd": "${0:make}"
+}
+    """)
+    template_type = "snippet"
 
 
-class NewSnippetCommand(sublime_plugin.WindowCommand):
-    def run(self) -> None:
-        v = self.window.new_file()
-        v.settings().set("default_dir", ST_USER_DIR)
-        v.settings().set("default_extension", "sublime-snippet")
-        v.assign_syntax("Packages/XML/XML.sublime-syntax")
+class NewPluginCommand(AbstractNewFileCommand):
+    save_path: Path = ST_USER_DIR / "untitled.py"
+    template = reformat("""
+from __future__ import annotations
 
-        template = reformat(
-            """
-            <snippet>
-            \t<content><![CDATA[
-            Hello, \\${1:this} is a \\${2:snippet}.
-            ]]></content>
-            \t<!-- Optional: Set a tabTrigger to define how to trigger the snippet -->
-            \t<!-- <tabTrigger>hello</tabTrigger> -->
-            \t<!-- Optional: Set a scope to limit where the snippet will trigger -->
-            \t<!-- <scope>source.python</scope> -->
-            </snippet>
-            """
-        )
-        v.run_command("insert_snippet", {"contents": template})
+import sublime
+import sublime_plugin
 
 
-class NewSyntaxCommand(sublime_plugin.WindowCommand):
-    def run(self) -> None:
-        v = self.window.new_file()
-        v.settings().set("default_dir", ST_USER_DIR)
-        v.settings().set("default_extension", "sublime-syntax")
-        v.assign_syntax("Packages/YAML/YAML.sublime-syntax")
+class ExampleCommand(sublime_plugin.TextCommand):
+\tdef run(self, edit: sublime.Edit) -> None:
+\t\t${0:self.view.insert(edit, 0, "Hello, World!")}
+    """)
+    template_type = "snippet"
 
-        template = reformat(
-            R"""
-            %YAML 1.2
-            ---
-            # See http://www.sublimetext.com/docs/syntax.html
-            name: Example C
-            version: 2
-            file_extensions:
-              - ec
-            scope: source.example-c
-            contexts:
-              main:
-                # Strings begin and end with quotes, and use backslashes as an escape
-                # character
-                - match: '"'
-                  scope: punctuation.definition.string.begin.example-c
-                  push: double_quoted_string
 
-                # Comments begin with a '//' and finish at the end of the line
-                - match: '//'
-                  scope: punctuation.definition.comment.example-c
-                  push: line_comment
+class NewSnippetCommand(AbstractNewFileCommand):
+    save_path: Path = ST_USER_DIR / "untitled.sublime-snippet"
+    template = reformat("""
+<snippet>
+\t<content><![CDATA[
+Hello, \\${1:this} is a \\${2:snippet}.
+]]></content>
+\t<!-- Optional: Set a tabTrigger to define how to trigger the snippet -->
+\t<!-- <tabTrigger>hello</tabTrigger> -->
+\t<!-- Optional: Set a scope to limit where the snippet will trigger -->
+\t<!-- <scope>source.python</scope> -->
+</snippet>
+    """)
+    template_type = "snippet"
 
-                # Keywords are if, else for and while.
-                # Note that blackslashes don't need to be escaped within single quoted
-                # strings in YAML. When using single quoted strings, only single quotes
-                # need to be escaped: this is done by using two single quotes next to each
-                # other.
-                - match: '\b(if|else|for|while)\b'
-                  scope: keyword.control.example-c
 
-                # Numbers
-                - match: '\b(-)?[0-9.]+\b'
-                  scope: constant.numeric.example-c
+class NewSyntaxCommand(AbstractNewFileCommand):
+    save_path: Path = ST_USER_DIR / "untitled.sublime-syntax"
+    template = reformat(R"""
+%YAML 1.2
+---
+# See http://www.sublimetext.com/docs/syntax.html
+name: Example C
+version: 2
+file_extensions:
+  - ec
+scope: source.example-c
+contexts:
+  main:
+    # Strings begin and end with quotes, and use backslashes as an escape
+    # character
+    - match: '"'
+      scope: punctuation.definition.string.begin.example-c
+      push: double_quoted_string
 
-              double_quoted_string:
-                - meta_scope: string.quoted.double.example-c
-                - match: '\\.'
-                  scope: constant.character.escape.example-c
-                - match: '"'
-                  scope: punctuation.definition.string.end.example-c
-                  pop: 1
+    # Comments begin with a '//' and finish at the end of the line
+    - match: '//'
+      scope: punctuation.definition.comment.example-c
+      push: line_comment
 
-              line_comment:
-                - meta_scope: comment.line.example-c
-                - match: $
-                  pop: 1
-            """
-        )
+    # Keywords are if, else for and while.
+    # Note that blackslashes don't need to be escaped within single quoted
+    # strings in YAML. When using single quoted strings, only single quotes
+    # need to be escaped: this is done by using two single quotes next to each
+    # other.
+    - match: '\b(if|else|for|while)\b'
+      scope: keyword.control.example-c
 
-        v.run_command("append", {"characters": template})
+    # Numbers
+    - match: '\b(-)?[0-9.]+\b'
+      scope: constant.numeric.example-c
+
+  double_quoted_string:
+    - meta_scope: string.quoted.double.example-c
+    - match: '\\.'
+      scope: constant.character.escape.example-c
+    - match: '"'
+      scope: punctuation.definition.string.end.example-c
+      pop: 1
+
+  line_comment:
+    - meta_scope: comment.line.example-c
+    - match: $
+      pop: 1
+    """)
