20143ba64a06e66fad56f6cd35c65c49bee90fd6
Jack Cherng <jfcherng@gmail.com>
2024-11-07 11:45:25 +0800

    chore: update LSP
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ typings/LSP/plugin/core/edit.pyi
@@ -0,0 +1,10 @@
+import sublime
+from ...protocol import AnnotatedTextEdit, Position as Position, TextEdit, WorkspaceEdit as WorkspaceEdit
+from .logging import debug as debug
+from .protocol import UINT_MAX as UINT_MAX
+
+WorkspaceChanges = dict[str, tuple[list[TextEdit | AnnotatedTextEdit], str | None, int | None]]
+
+def parse_workspace_edit(workspace_edit: WorkspaceEdit, label: str | None = None) -> WorkspaceChanges: ...
+def parse_range(range: Position) -> tuple[int, int]: ...
+def apply_text_edits(view: sublime.View, edits: list[TextEdit] | None, *, label: str | None = None, process_placeholders: bool | None = False, required_view_version: int | None = None) -> None: ...
