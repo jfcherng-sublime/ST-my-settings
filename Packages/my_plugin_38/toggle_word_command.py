d4b2c668e90af0cfb95def861dedabbe2e3313a8
Jack Cherng <jfcherng@gmail.com>
2025-01-03 15:38:43 +0800

    refactor: use deps for my_plugin_38
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- Packages/my_plugin_38/toggle_word_command.py
+++ Packages/my_plugin_38/toggle_word_command.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
 import re
-from collections.abc import Iterable, Iterator
-from dataclasses import dataclass, field
+from collections import UserList
+from collections.abc import Iterable
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
 
 import sublime
 import sublime_plugin
@@ -11,50 +13,58 @@ import sublime_plugin
 @dataclass
 class WordInfo:
     word: str
-    next_: WordInfo = field(init=False)
-
-
-class ToggleGroup:
-    def __init__(self, words: Iterable[str] | None = None) -> None:
-        self._word_infos: list[WordInfo] = []
-        self.extend(words or [])
-
-    def __iter__(self) -> Iterator[WordInfo]:
-        return iter(self._word_infos)
+    next_: WordInfo | None = None
 
     def __len__(self) -> int:
-        return len(self._word_infos)
+        return len(self.word)
+
+    def __next__(self) -> WordInfo:
+        if self.next_ is None:
+            raise StopIteration
+        return self.next_
+
+    def __str__(self) -> str:
+        return self.word
+
+
+if TYPE_CHECKING:
+    # requires py39+
+    WordInfoUserList = UserList[WordInfo]
+else:
+    WordInfoUserList = UserList
+
+
+class ToggleGroup(WordInfoUserList):
+    def __init__(self, words: Iterable[str] | None = None) -> None:
+        super().__init__()
+        self.extend(words or [])
 
     def append(self, word: str) -> None:
         word_info = WordInfo(word)
-        # make `self.word_infos` circular
-        if self._word_infos:
-            self._word_infos[-1].next_ = word_info
-        self._word_infos.append(word_info)
-        self._word_infos[-1].next_ = self._word_infos[0]
+        # make `self.data` circular
+        if self.data:
+            self.data[-1].next_ = word_info
+        self.data.append(word_info)
+        self.data[-1].next_ = self.data[0]
 
     def extend(self, words: Iterable[str]) -> None:
         for word in words:
             self.append(word)
 
 
-class ToggleGroups:
+class ToggleGroupCollection:
     def __init__(self, group_words: list[Iterable[str]]) -> None:
-        groups = [ToggleGroup(words) for words in filter(None, group_words)]
+        self.groups = [ToggleGroup(words) for words in filter(None, group_words)]
 
-        # sort words by their length, so that longer words are tried first
-        self._word_infos: list[WordInfo] = sorted(
-            (word_info for group in groups for word_info in group),
-            key=lambda word_info: len(word_info.word),
-            reverse=True,
-        )
+        self.sorted_word_infos: list[WordInfo] = []
+        self._sort_word_infos()
 
-    def is_empty(self) -> bool:
-        return not self._word_infos
+    def __bool__(self) -> bool:
+        return bool(self.groups)
 
     @property
-    def longest_word_length(self) -> int:
-        return len(self._word_infos[0].word) if self._word_infos else 0
+    def longest_word(self) -> str:
+        return self.sorted_word_infos[0].word if self.sorted_word_infos else ""
 
     def find(
         self,
@@ -64,33 +74,44 @@ class ToggleGroups:
         caret_pt: int,
     ) -> tuple[WordInfo, sublime.Region] | None:
         string = view.substr(search_region)
-        for word_info in self._word_infos:
+        for word_info in self.sorted_word_infos:
             for m in re.finditer(re.escape(word_info.word), string, flags=re.IGNORECASE):
                 m_a, m_b = m.span()
                 if (found_region := sublime.Region(search_region.a + m_a, search_region.a + m_b)).contains(caret_pt):
                     return word_info, found_region
         return None
 
+    def _sort_word_infos(self) -> None:
+        """
+        Extract all `word_info`s, whose `next_` has pointed to its next `word_info`.
+        Sort them by their lengths, so that longer words are tried first later.
+        """
+        self.sorted_word_infos = sorted(
+            (word_info for group in self.groups for word_info in group),
+            key=len,
+            reverse=True,
+        )
+
 
 class ToggleWordCommand(sublime_plugin.TextCommand):
     def run(self, edit: sublime.Edit) -> None:
         preferences = sublime.load_settings("Preferences.sublime-settings")
 
-        if (toggle_groups := ToggleGroups(preferences.get("toggle_word.groups", []))).is_empty():
+        if not (group_collection := ToggleGroupCollection(preferences.get("toggle_word.groups", []))):
             return
 
-        for region in self.view.sel():
+        for region in reversed(self.view.sel()):
             region = sublime.Region(region.begin(), region.end())
 
             if (search_region := region).empty():
-                search_radius = toggle_groups.longest_word_length
+                search_radius = len(group_collection.longest_word)
                 search_region = sublime.Region(region.a - search_radius, region.b + search_radius)
 
             self._toggle_word(
                 self.view,
                 edit,
                 source_region=search_region,
-                toggle_groups=toggle_groups,
+                group_collection=group_collection,
                 caret_pt=region.a,
             )
 
@@ -100,14 +121,14 @@ class ToggleWordCommand(sublime_plugin.TextCommand):
         edit: sublime.Edit,
         *,
         source_region: sublime.Region,
-        toggle_groups: ToggleGroups,
+        group_collection: ToggleGroupCollection,
         caret_pt: int = -1,
     ) -> None:
-        if not (find_result := toggle_groups.find(view, search_region=source_region, caret_pt=caret_pt)):
+        if not (find_result := group_collection.find(view, search_region=source_region, caret_pt=caret_pt)):
             return
-
         word_info, source_region = find_result
-        next_word_info = word_info.next_
+
+        next_word_info = next(word_info)
         source_text = view.substr(source_region)
 
         for set_case in (str, str.lower, str.upper, str.title):
